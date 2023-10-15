3710ddfd4bb71ca66cea8ece50aed58b5a5c2d73
Bardia <bilbeigitimouri@my.bcit.ca>
2023-10-14 18:25:27 -0700

    Created abstract HttpServlet.

--- src/HttpServlet.java
+++ src/HttpServlet.java
@@ -1,2 +1,5 @@
-public interface HttpServlet {
+public abstract class HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) { return; };
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) { return; };
+
 }
